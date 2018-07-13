944e4eda425818431444902de4d6294e55f9cbaa
Kummara Shivatej <kshivatej@asiapac.globalcsc.net>
2018-07-13 19:26:33 +0530

    Added search page

--- src/main/java/com/test/pages/LoginPage.java
+++ src/main/java/com/test/pages/LoginPage.java
@@ -3,5 +3,10 @@ package com.test.pages;
 public class LoginPage {
 	public void LoginPage(){
 		System.out.println("LoginPage");
+		
+		}
+	
+	public void LoginSet(){
+		System.out.println("Login page set method");
 	}
 }
---
+++ src/main/java/com/test/pages/SearchPage.java
@@ -0,0 +1,9 @@
+package com.test.pages;
+
+public class SearchPage {
+	
+	public void SearchPage(){
+		System.out.println("Search Page");
+	}
+
+}
