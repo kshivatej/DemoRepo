03c24afb122ab7472a634b2a2b16638c7590958d
kshivatej <41186118+kshivatej@users.noreply.github.com>
2018-07-13 19:50:17 +0530

    Update LoginPage.java
    modifying Loginpage  the code

--- src/main/java/com/test/pages/LoginPage.java
+++ src/main/java/com/test/pages/LoginPage.java
@@ -2,11 +2,11 @@ package com.test.pages;
 
 public class LoginPage {
 	public void LoginPage(){
-		System.out.println("LoginPage");
+		System.out.println("LoginPage_edited");
 		
 		}
 	
 	public void LoginSet(){
-		System.out.println("Login page set method");
+		System.out.println("Login page set method_edited");
 	}
 }
